6a1056f8204b4c0be5c6cd4f5eca9be80a38e500
Carlos Loureda <balanzeneto@gmail.com>
2020-02-24 18:41:44 +0100

    [refactor] Removed tablet.scss and mobile.scss
    Media queries moved to style.scss and weather-card.scss

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,5 @@
 import "./styles/reset.scss";
 import "./styles/style.scss";
-import "./styles/mobile.scss";
-import "./styles/tablet.scss";
 import "./styles/weather-card.scss";
 import "./styles/modal.scss";
 
